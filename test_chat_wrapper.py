9af59dcd6714098a2efee01abd666927553727a5
Oliver Hough <hello@oliverhough.io>
2023-05-08 00:15:18 +0200

    test primitive LOADFILE hooks

--- test_chat_wrapper.py
+++ test_chat_wrapper.py
@@ -29,30 +29,29 @@ userMessage = "check our financials"
 messages.append({"role": "user", "content": userMessage})
 print("Human: " + userMessage)
 response = get_response(messages)
-print(response.choices[0].message.content)
-messages.append(response.choices[0].message)
-
-
-# attempt to parse the response
-print("attempting to parse the response")
-print("The response contains " + str(len(response.choices[0].message.content.splitlines())) + " lines")
-# split the response into in a list os strings, the command shold be at index 0
-lstres = response.choices[0].message.content.split()
-if lstres[0] == "LOADFILE":
-    print("LOADFILE command detected")
-    print("The file path is " + lstres[1])
-    print("attempting to open the file")
-    file = Path( ".", lstres[1])
-    filecontents = file.read_text()
-    print("Human: " + filecontents)
-    messages.append({"role": "user", "content": filecontents})
-    response = get_response(messages)
-    print(response.choices[0].message.content)
-    print("end test")
-else:
-    print("LOADFILE command not detected, loading complete or not needed" + lstres[0])
-    print(response.choices[0].message.content)
-    # attempt to parse the response
+loadloop = True
+while loadloop == True:
+    #print(response.choices[0].message.content)
+    messages.append(response.choices[0].message)
+    #attempt to parse the response
     print("attempting to parse the response")
     print("The response contains " + str(len(response.choices[0].message.content.splitlines())) + " lines")
-    print(response.choices[0].message.content)
+    # split the response into in a list os strings, the command shold be at index 0
+    lstres = response.choices[0].message.content.split()
+    if lstres[0] == "LOADFILE":
+        print("LOADFILE command detected")
+        print("The file path is " + lstres[1])
+        print("attempting to open the file")
+        file = Path( ".", lstres[1])
+        filecontents = file.read_text()
+        print("Human: " + filecontents)
+        messages.append({"role": "user", "content": filecontents})
+        response = get_response(messages)
+    else:
+        print("LOADFILE command not detected, loading complete or not needed")
+        #print(response.choices[0].message.content)
+        # attempt to parse the response
+        print("attempting to parse the response")
+        print("The response contains " + str(len(response.choices[0].message.content.splitlines())) + " lines")
+        print(response.choices[0].message.content)
+        loadloop = False
